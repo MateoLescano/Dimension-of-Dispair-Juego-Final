284811621b91d28ff5834a503ea82b2d18560251
Manuel Núñez <manuelnuniezz@gmail.com>
2022-02-12 14:19:23 -0300

    Frame de invulnerabilidad
    el personaje parpadea cuando recibe daño.
    es algo simplemente visual. en un futuro estaria bueno agregar de que el personaje no reciba daño en ese periodo

---
+++ Assets/Scripts/Collision_Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision_Player : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Damage"))
+        {
+            anim.SetTrigger("TakeDamageTrigger");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
