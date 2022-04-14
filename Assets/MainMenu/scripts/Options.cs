70843d58e3246a332b22fcb5ab94308b5ba23d05
espinaculo <96502889+espinaculo@users.noreply.github.com>
2022-04-13 21:50:32 -0300

    menu de opciones casi terminado

--- Assets/MainMenu/scripts/Options.cs
+++ Assets/MainMenu/scripts/Options.cs
@@ -1,28 +1,65 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Options : MonoBehaviour
 {
     public GameObject mainCanvas;
     public GameObject optionsCanvas;
-    public Toggle screen;
+    [SerializeField] private AudioMixer audioMixer;
+
     
 
+    Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropdown;
 
+
+    private void Awake()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].height ==Screen.currentResolution.height && resolutions[i].width == Screen.currentResolution.width)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
 
     public void GoToOptions()
     {
-        mainCanvas.SetActive(false);
         optionsCanvas.SetActive(true);
+        mainCanvas.SetActive(false);
+        
     }
 
-    public void Fullscreen()
+    public void Fullscreen(bool fullscreen)
     {
        
-        Screen.fullScreen = !Screen.fullScreen;
+        Screen.fullScreen = fullscreen;
         Debug.Log(Screen.fullScreen);
+    }
+
+    public void SetMasterVolume(float sliderValue)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20f);
     }
 }
